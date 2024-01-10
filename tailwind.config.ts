3fecbfdfb8d522c9c5b286b37b6c499a34543f2e
Giovana de Miranda <giovana.miranda@bitize.com.br>
2024-01-10 16:16:54 -0300

    style: add variaveis no tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'base-input': '#040f1a',
+        'base-background': '#071422',
+        'base-profile': '#0b1b2b',
+        'base-post': '#112131',
+        'base-border': '#1c2f41',
+        'base-label': '#3a536b',
+        'base-span': '#7b96b2',
+        'base-text': '#afc2d4',
+        'base-subtitle': '#c4d4e3',
+        'base-title': '#e7edf4',
+        'brand-blue': '#3294f8',
+      },
+      fontFamily: {
+        'Nunito': ['Nunito', 'cursive'],
+      },
     },
   },
   plugins: [],
