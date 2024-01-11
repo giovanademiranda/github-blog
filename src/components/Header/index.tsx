5e4367bc642897c21ee6a6e1f63add3fb83b66f6
Giovana de Miranda <giovana.miranda@bitize.com.br>
2024-01-11 16:01:40 -0300

    feat: add profile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
 import background from '../../assets/background.svg';
+import Profile from '../Profile';
 
 export default function Header() {
   return (
-    <header className="w-full flex max-w-7xl justify-center items-center">
-      <div className="flex">
+    <header className="w-full flex justify-center items-center">
+      <div className='flex flex-col items-center justify-center'>
         <Image src={background} alt="Background" />
+        <div className="flex justify-center items-center relative -top-24">
+          <Profile />
+        </div>
       </div>
     </header >
   )
