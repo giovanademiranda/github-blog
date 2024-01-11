27896d9e78297b97ba7f27ec85eebf3c6f44541d
Giovana de Miranda <giovana.miranda@bitize.com.br>
2024-01-11 17:02:10 -0300

    feat: add Blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Card from "@/components/Card";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="max-w-7xl w-full flex flex-col items-center justify-center">
+      <div className="w-[65%] flex flex-col gap-3">
+        <div className="flex flex-row justify-between">
+          <h2 className="font-medium text-lg text-base-subtitle">Publicações</h2>
+          <p className="text-sm text-base-span">6 publicações</p>
+        </div>
+        <input type="text" placeholder="Buscar conteúdo" className="bg-base-input border-base-border px-4 py-3 rounded-md .placeholder-base-label::placeholder text-base-text font-normal text-base outline-none border focus:border-brand-blue" />
+      </div>
+      <div className="w-[65%] flex justify-center mt-10 md:justify-between gap-9 md:gap-y-4">
+        <Card />
+      </div>
     </div>
   )
 }
