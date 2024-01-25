40f3a4676fb84287ada653005bde625a5d55279d
Giovana de Miranda <giovana.miranda@bitize.com.br>
2024-01-25 14:55:32 -0300

    fix: profile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,18 +12,7 @@ export interface HeaderProps {
 
 
 export default function Header({ type, userProfile, userPost }: HeaderProps) {
-  const profileContent = userProfile && (
-    <Profile
-      avatar_url={userProfile.avatar_url}
-      name={userProfile.name}
-      username={userProfile.login}
-      followers={userProfile.followers_url}
-      company={userProfile.company}
-      body={userProfile.body}
-    />
-  );
-
-  const content = type === 'post' ? <PostInfo post={userPost} profile={userProfile} /> : profileContent;
+  const content = type === 'post' ? <PostInfo post={userPost} profile={userProfile} /> : <Profile profile={userProfile} />;
 
   return (
     <header className="w-full flex justify-center items-center">
